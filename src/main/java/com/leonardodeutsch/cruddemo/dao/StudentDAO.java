f684f7a0369b073c08eb6fb842413ac8d7b587de
Leo <deutschleonardo@gmail.com>
2023-11-17 13:46:00 -0600

    Implemented the findAll method which returns a list of all students, in descending (Z-A) alphabetical order.

--- src/main/java/com/leonardodeutsch/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/leonardodeutsch/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.leonardodeutsch.cruddemo.dao;
 
 import com.leonardodeutsch.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
