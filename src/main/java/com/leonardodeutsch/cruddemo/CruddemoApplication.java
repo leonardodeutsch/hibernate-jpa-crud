ad2dbb1be732452a5b8bd06109876a392ffa8289
Leo <deutschleonardo@gmail.com>
2023-11-17 12:49:22 -0600

    Added the findById method to the studentDAO, tested by creating a student and retrieving it by using its id.

--- src/main/java/com/leonardodeutsch/cruddemo/CruddemoApplication.java
+++ src/main/java/com/leonardodeutsch/cruddemo/CruddemoApplication.java
@@ -17,9 +17,29 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner (StudentDAO studentDAO) {
 
-		return runner -> {
-			createStudent(studentDAO);
-		};
+		return runner -> readStudent(studentDAO);
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create a student object
+		System.out.println("Creating new student object...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@gmail.com");
+
+		// save the student
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		System.out.println("Found the student: " + myStudent);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
--- src/main/java/com/leonardodeutsch/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/leonardodeutsch/cruddemo/dao/StudentDAO.java
@@ -4,4 +4,6 @@ import com.leonardodeutsch.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student theStudent);
+
+    Student findById(Integer id);
 }
